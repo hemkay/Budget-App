51e970391258d2fc75b2a059ca736e34351334ca
hemkay <imukaila@infinion.co>
2021-04-16 12:30:26 +0100

    Use a try except block to catch string amount

--- BudgetClass.py
+++ BudgetClass.py
@@ -1,25 +1,33 @@
 class Budget():
     def __init__(self, category):
         self.category = category
-        self.balance = 0
+        self.balance = 0.0
 
-    def withdraw(self, int: amount):
+    def withdraw(self, amount):
+        try:
+            amount = float(amount)
+        except ValueError as e:
+            print(e)
+            return None
         if self.balance < amount:
             print(f"The balance is too low to withdraw {amount}, you have a balance of {self.balance}")
         else:
             self.balance -= amount
             print(f"{amount} has been withdrawn.")
-    def deposit(self, int: amount):
+    
+    
+    def deposit(self, amount):
+        try:
+            amount = float(amount)
+        except ValueError as e:
+            print(e)
+            return None
         self.balance += amount
         print(f"{amount} has been deposited.")
-    def transfer_balance(self, Budget: budget_cat):
+    
+    
+    def transfer_balance(self, budget_cat):
         budget_cat.balance += self.balance
         self.balance = 0
         print(f" You have transfered the balance for budget of {budget_cat.category} to {self.category}")
 
-entertainment = Budget('entertainment')
-food = Budget('food')
-food.deposit()
-food.transfer_balance(entertainment)
-print(entertainment.balance, entertainment.category)
-print(food.balance, food.category)
--- Categories.py
+++ Categories.py
@@ -1,5 +1,7 @@
-import Budget
+from BudgetClass import Budget
 
 entertainment = Budget('Entertainment')
 food = Budget('Food')
 clothing = Budget('Clothing')
+entertainment.deposit('20w')
+print(entertainment.balance)
